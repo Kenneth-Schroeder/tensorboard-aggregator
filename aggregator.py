6f217f858c76fd68602571fdbc647ffeadef0d68
Kenneth-Schroeder <kenneth3x10@web.de>
2023-08-24 11:46:00 +0200

    just convert tensorboard files to usable csv format without aggregation

--- aggregator.py
+++ aggregator.py
@@ -16,105 +16,103 @@ import tensorflow as tf
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 from tensorflow.core.util.event_pb2 import Event
 
-FOLDER_NAME = 'aggregates'
+FOLDER_NAME = 'csvs'
 
+def get_items_if_key(scalar_accumulator, key):
+    try:
+        return scalar_accumulator.Items(key)
+    except KeyError:
+        return []
+    
+def extract_singles(dpath, subpath):
+    accumulators = [EventAccumulator(str(dpath / dname / subpath)).Reload() for dname in os.listdir(dpath) if dname != FOLDER_NAME]
 
-def extract(dpath, subpath):
-    scalar_accumulators = [EventAccumulator(str(dpath / dname / subpath)).Reload(
-    ).scalars for dname in os.listdir(dpath) if dname != FOLDER_NAME]
-
+    scalar_accumulators = [acc.scalars for acc in accumulators]
     # Filter non event files
     scalar_accumulators = [scalar_accumulator for scalar_accumulator in scalar_accumulators if scalar_accumulator.Keys()]
 
     # Get and validate all scalar keys
-    all_keys = [tuple(scalar_accumulator.Keys()) for scalar_accumulator in scalar_accumulators]
-    assert len(set(all_keys)) == 1, "All runs need to have the same scalar keys. There are mismatches in {}".format(all_keys)
-    keys = all_keys[0]
+    all_keys = [key for scalar_accumulator in scalar_accumulators for key in scalar_accumulator.Keys()]
+    keys = list(set(all_keys))
 
-    all_scalar_events_per_key = [[scalar_accumulator.Items(key) for scalar_accumulator in scalar_accumulators] for key in keys]
+    all_scalar_events_per_key = [[get_items_if_key(scalar_accumulator, key) for scalar_accumulator in scalar_accumulators] for key in keys]
 
     # Get and validate all steps per key
-    all_steps_per_key = [[tuple(scalar_event.step for scalar_event in scalar_events) for scalar_events in all_scalar_events]
+    all_steps_per_key = [[[scalar_event.step for scalar_event in run_events] for run_events in all_scalar_events]
                          for all_scalar_events in all_scalar_events_per_key]
 
-    for i, all_steps in enumerate(all_steps_per_key):
-        assert len(set(all_steps)) == 1, "For scalar {} the step numbering or count doesn't match. Step count for all runs: {}".format(
-            keys[i], [len(steps) for steps in all_steps])
 
-    steps_per_key = [all_steps[0] for all_steps in all_steps_per_key]
+    # keys = table names / identifiers
+    # steps per key = total steps recorded per key (might differ from run to run - some are even 0) [num_keys, number_of_runs]
+    # wall_times_per_key = average wall times per step per key (one value per key) [num_keys, number_of_runs, num_scalars]
+    # scalars_per_key_n_run_n = [num_keys, number_of_runs, num_scalars]
+
+    steps_per_key_n_run = all_steps_per_key
 
     # Get and average wall times per step per key
-    wall_times_per_key = [np.mean([tuple(scalar_event.wall_time for scalar_event in scalar_events) for scalar_events in all_scalar_events], axis=0)
+    wall_times_per_key_n_run_n_scalar = [[[scalar_event.wall_time for scalar_event in run_events] for run_events in all_scalar_events]
                           for all_scalar_events in all_scalar_events_per_key]
 
     # Get values per step per key
-    values_per_key = [[[scalar_event.value for scalar_event in scalar_events] for scalar_events in all_scalar_events]
+    scalars_per_key_n_run_n = [[[scalar_event.value for scalar_event in run_events] for run_events in all_scalar_events]
                       for all_scalar_events in all_scalar_events_per_key]
 
-    all_per_key = dict(zip(keys, zip(steps_per_key, wall_times_per_key, values_per_key)))
+    # run folders with one file per key
+    #run_counter = 0
+    #for acc in accumulators:
+    #    if acc.scalars.Keys():
+    #        dname = acc.path.split('/')[-1]
+    #        assert(dname != ".DS_Store")
+    #        if dname != FOLDER_NAME:
+    #            for j, key in enumerate(keys):
+    #                csv_dict = {}
+    #                if len(steps_per_key_n_run[j][run_counter]) > 0:
+    #                    csv_dict[key] = scalars_per_key_n_run_n[j][run_counter]
+    #                    csv_dict['Steps'] = steps_per_key_n_run[j][run_counter]
+    #                    csv_dict['Wall_Time'] = wall_times_per_key_n_run_n_scalar[j][run_counter]
+    #                if csv_dict:
+    #                    write_single_csv(dpath / FOLDER_NAME, dname, key, csv_dict)
+    #            run_counter+=1
 
-    return all_per_key
-
-
-def aggregate_to_summary(dpath, aggregation_ops, extracts_per_subpath):
-    for op in aggregation_ops:
-        for subpath, all_per_key in extracts_per_subpath.items():
-            path = dpath / FOLDER_NAME / op.__name__ / dpath.name / subpath
-            aggregations_per_key = {key: (steps, wall_times, op(values, axis=0)) for key, (steps, wall_times, values) in all_per_key.items()}
-            write_summary(path, aggregations_per_key)
-
-
-def write_summary(dpath, aggregations_per_key):
-    writer = tf.summary.create_file_writer(str(dpath))
-
-    for key, (steps, wall_times, aggregations) in aggregations_per_key.items():
-        for step, wall_time, aggregation in zip(steps, wall_times, aggregations):
-            with writer.as_default():
-                tf.summary.scalar(key, aggregation, step=step)
-                writer.flush()
-
-def aggregate_to_csv(dpath, aggregation_ops, extracts_per_subpath):
-    for subpath, all_per_key in extracts_per_subpath.items():
-        for key, (steps, wall_times, values) in all_per_key.items():
-            aggregations = [op(values, axis=0) for op in aggregation_ops]
-            write_csv(dpath, subpath, key, dpath.name, aggregations, steps, aggregation_ops)
+    # key folders with one file per run
+    for j, key in enumerate(keys):
+        run_counter = 0
+        for acc in accumulators:
+            if acc.scalars.Keys():
+                dname = acc.path.split('/')[-1]
+                assert(dname != ".DS_Store")
+                if dname != FOLDER_NAME:
+                    csv_dict = {}
+                    if len(steps_per_key_n_run[j][run_counter]) > 0:
+                        csv_dict[key] = scalars_per_key_n_run_n[j][run_counter]
+                        csv_dict['Steps'] = steps_per_key_n_run[j][run_counter]
+                        csv_dict['Wall_Time'] = wall_times_per_key_n_run_n_scalar[j][run_counter]
+                    if csv_dict:
+                        write_single_csv(dpath / FOLDER_NAME, key, dname, csv_dict)
+                    run_counter+=1
 
 
 def get_valid_filename(s):
     s = str(s).strip().replace(' ', '_')
     return re.sub(r'(?u)[^-\w.]', '', s)
 
-
-def write_csv(dpath, subpath, key, fname, aggregations, steps, aggregation_ops):
-    path = dpath / FOLDER_NAME
+def write_single_csv(output_path, folder_name, file_title, data_dict):
+    path = output_path / get_valid_filename(folder_name)
 
     if not path.exists():
         os.makedirs(path)
 
-    file_name = get_valid_filename(key) + '-' + get_valid_filename(subpath) + '-' + fname + '.csv'
-    aggregation_ops_names = [aggregation_op.__name__ for aggregation_op in aggregation_ops]
-    df = pd.DataFrame(np.transpose(aggregations), index=steps, columns=aggregation_ops_names)
+    file_name = get_valid_filename(file_title) + '.csv'
+    df = pd.DataFrame(data_dict)
     df.to_csv(path / file_name, sep=';')
 
-
-def aggregate(dpath, output, subpaths):
+def collect_csvs(dpath, output, subpaths):
     name = dpath.name
 
     aggregation_ops = [np.mean, np.min, np.max, np.median, np.std, np.var]
 
-    ops = {
-        'summary': aggregate_to_summary,
-        'csv': aggregate_to_csv
-    }
-
-    print("Started aggregation {}".format(name))
-
-    extracts_per_subpath = {subpath: extract(dpath, subpath) for subpath in subpaths}
-
-    ops.get(output)(dpath, aggregation_ops, extracts_per_subpath)
-
-    print("Ended aggregation {}".format(name))
-
+    for subpath in subpaths:
+        extract_singles(dpath, subpath)
 
 if __name__ == '__main__':
     def param_list(param):
@@ -144,4 +142,4 @@ if __name__ == '__main__':
     if args.output not in ['summary', 'csv']:
         raise argparse.ArgumentTypeError("Parameter {} is not summary or csv".format(args.output))
 
-    aggregate(path, args.output, args.subpaths)
+    collect_csvs(path, args.output, args.subpaths)
